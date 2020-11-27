a72efad7c35995dd4db31b3c8d97fb82b157172e
linjiawei <linjiav@outlook.com>
2020-11-27 21:26:01 +0800

    add an index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 
+  get '/index', to: 'index#new'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
