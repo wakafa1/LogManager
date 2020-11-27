764aa0f716fa9f97cecc7af5f94e57e6f43f7c92
wangkaifan <wangkaifan@ict.ac.cn>
2020-11-27 14:13:24 +0800

    upload: extract filename as class varible

--- app/controllers/uploader_controller.rb
+++ app/controllers/uploader_controller.rb
@@ -8,12 +8,12 @@ class UploaderController < ApplicationController
 
     # Set ./public/upload/ as upload folder. If not exist, create it
     @upload_folder = Rails.root.join('public','uploads')
+    @upload_filename = Rails.root.join(@upload_folder, @uploaded_io.original_filename)
     FileUtils.makedirs(@upload_folder) unless File.exists?@upload_folder
 
     # Write file to @upload_folder/original_filename
-    File.open(Rails.root.join(@upload_folder, @uploaded_io.original_filename), 'wb') do |file|
+    File.open(@upload_filename, 'wb') do |file|
       file.write(@uploaded_io.read)
     end
-
   end
 end
