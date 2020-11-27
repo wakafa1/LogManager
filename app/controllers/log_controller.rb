c8141b650218f7540b7bebcfc32689df0e0ba0ac
wangkaifan <wangkaifan@ict.ac.cn>
2020-11-27 15:55:08 +0800

    log: add security check

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -1,5 +1,8 @@
 class LogController < ApplicationController
+  include LogHelper
+
   def profile
+    !check_permission ? return :
     @current_log = Log.find(params[:id])
   end
 end
--- app/helpers/log_helper.rb
+++ app/helpers/log_helper.rb
@@ -1,2 +1,11 @@
 module LogHelper
+  include SessionsHelper
+  def check_permission
+    if current_user.nil?
+      redirect_to :login
+      false
+    else
+      true
+    end
+  end
 end
